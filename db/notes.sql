47cb07671c8ba22c8b2007aeb9a9c99c4f994a7a
girvinjunod <girvinjunod@gmail.com>
2022-06-02 19:45:43 +0700

    week 7

---
+++ db/notes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id int AUTO_INCREMENT ,
+    username varchar(255),
+    password varchar(255),
+    PRIMARY KEY (id)
+);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const dotenv = require("dotenv");
 const app = express();
 const port = 3000;
@@ -15,13 +17,12 @@ dotenv.config();
 app.use(express.json());
 // request logging
 app.use(morgan("dev"));
-
 const connection = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "admin_test",
-  password: "csbackend",
-  database: "test",
+  host: process.env.dbhost,
+  user: process.env.dbuser,
+  password: process.env.dbpassword,
+  database: process.env.dbname,
   // port: 3340,
 });
 
@@ -30,9 +31,60 @@ app.get("/", (req, res) => {
   res.send({ err: false, msg: "Hello World!" });
 });
 
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  connection.query(
+    "SELECT * from users where username=? limit 1",
+    [username],
+    (err, rows) => {
+      if (err || rows.length === 0) {
+        res.status(400).send({ msg: "Username doesn't exist", err: true });
+        return;
+      }
+      bcrypt.compare(password, rows[0].password, function (err, result) {
+        // result == true
+        if (result) {
+          let token = generateAccessToken(username);
+          let response = { err: false, msg: "Success", token: token };
+          res.status(200).send(response);
+          return;
+        } else {
+          res.status(400).send({ msg: "Wrong password", err: true });
+          return;
+        }
+      });
+    }
+  );
+});
+
+//insert data user
+app.post("/register", (req, res) => {
+  let { username, password } = req.body;
+
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      // Store hash in your password DB.
+      connection.query(
+        "INSERT INTO users (username, password) VALUES (?, ?)",
+        [username, hash],
+        (err, rows) => {
+          if (err) {
+            res.send({ msg: err.message, err: true });
+          }
+          if (rows) {
+            let response = { err: false, msg: "Success" };
+            res.send(response);
+          }
+        }
+      );
+    });
+  });
+  return;
+});
+
 app.get("/persons", authenticateToken, (req, res) => {
   let limit = req.query.limit || 100;
-  console.log(req.user);
 
   connection.query("SELECT * from person limit " + limit, (err, rows) => {
     if (err) {
@@ -64,26 +116,6 @@ app.get("/person/:id", (req, res) => {
       return;
     }
   });
-});
-
-//insert data person
-app.post("/register", (req, res) => {
-  let { lastName, firstName, Address, City } = req.body;
-
-  connection.query(
-    "INSERT INTO person (last_name, first_name, address, city) VALUES (?, ?, ?, ?)",
-    [lastName, firstName, Address, City],
-    (err, rows) => {
-      if (err) {
-        res.send({ msg: err.message, err: true });
-      }
-      if (rows) {
-        let response = { err: false, msg: "Success" };
-        res.send(response);
-      }
-    }
-  );
-  return;
 });
 
 //update data person
